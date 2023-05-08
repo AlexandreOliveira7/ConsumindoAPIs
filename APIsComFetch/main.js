cef9e298f67b147044bfcb2f510dc7c6f9736e04
Alexandre Oliveira <serolfalexandre@gmail.com>
2023-05-08 14:35:36 -0300

    training post and put with fetch!

--- APIsComFetch/main.js
+++ APIsComFetch/main.js
@@ -19,5 +19,47 @@ function getUser() {
     .catch(error => console.error(error))
 }
 
+function addUser(newUser) {
+   fetch(url, {
+     method: "POST",
+     body: JSON.stringify(newUser),
+     headers: {
+        "Content-type": "application/json;charset=UTF-8"
+     }
+   })
+     .then(response => response.json())
+     .then(data => alertApi.textContent = data)
+     .catch(error => consolo.error(error))
+}
+
+function updateUser(updatedUser) {
+    fetch(`${url}/1`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-type": "application/json;charset=UTF-8"
+        }
+    } )
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+const updatedUser = {
+    name: "Marcelo Clovis",
+    avatar:"http://picsum.photos/200/300",
+    city: "Recife"
+}
+
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "http://picsum.photos/200/300",
+    city: "São Paulo"
+}
+
+//addUser(newUser)
+
+updateUser(updatedUser)
+
 getUsers()
 getUser()
